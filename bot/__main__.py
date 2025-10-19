ecbf2fe6bd53cdb3944760b772efb7168e357ea1
Levhcwm21. <alexags1983@gmail.com>
2025-10-19 17:52:06 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,16 +1,7 @@
 import asyncio
 from traceback import format_exc
 from aiohttp import web
-
-asyncio.set_event_loop_policy(asyncio.DefaultEventLoopPolicy())
-try:
-    asyncio.get_event_loop()
-except RuntimeError:
-    asyncio.set_event_loop(asyncio.new_event_loop())
-# ------------------------------------------------
-
 from pyrogram import idle
-
 from bot import __version__, LOGGER
 from bot.config import Telegram
 from bot.server import web_server
